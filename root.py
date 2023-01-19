68e7b52d6ce806053a748ce13a64acb3145e619b
soudip-kundu <soudip kundu96@gmail.com>
2023-01-20 00:47:45 +0530

    tabular,bisection,newton-raphson

--- root.py
+++ root.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3nn
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 11 17:34:20 2023
+##!/usr/bin/env python3nn
+## -*- coding: utf-8 -*-
+#"""
+#Created on Wed Jan 11 17:34:20 2023
+#
+#@author: soudip
+#"""
+## numerical method to find root using tabular method
 
-@author: soudip
-"""
-# numerical method to find root using bisection method
 function=input("enter a function: ")
 print("first provide the interval where the root lies say [i,g] , input i,g ")
 i=int(input("enter i = "))
@@ -34,5 +35,49 @@ if c>0:
  print("the root is between ","%0.2f"%(n-a)," & ","%0.2f"%n)
 if c==0 and d==0:
     print("there is no solution within the range [",i,",",g,"]") 
-# another way to define a function is 
-#f = lambda x: 10**x + x - 4    
+    
+ #newton raphson method
+ 
+import numpy as np
+import math
+def f(x):
+  return 10**x + x - 4
+def df(x):
+  return math.log(10)*10**x+1
+a=float(input("enter the intital guess of the root :"))
+i=abs(f(a)/df(a))
+x=a
+while i>0.001:
+    x=x-i
+    i=abs(f(x)/df(x))
+print("%0.3f"%x)
+
+#bisection method
+
+import numpy as np
+import math
+function=input("enter a function: ")
+def f(x):
+  return eval(function)
+print("first provide the interval where the root lies say [i,g] , input i,g ")
+i=int(input("enter i = "))
+g=int(input("enter g = "))
+er=float(input("enter the tolerable error : "))
+mid=(i+g)/2
+while abs(i-g)>er:
+    if f(i)*f(mid)<0:
+        i=i
+        g=mid
+    else:
+        i=mid
+        g=g
+    mid=(i+g)/2   
+print("the root lies between ",i," and ",g)
+        
+    
+    
+    
+
+
+
+
